0c6553e54c4da173bf6386edb9b792434cf8852f
AntonyKon <kononenko150@gmail.com>
2021-07-22 17:32:10 +0300

    add some conditions to main.py

--- main.py
+++ main.py
@@ -9,16 +9,6 @@ from BuilderWithEncoding import BuilderWithEncoding
 from RRMS_checking import RRMS_calc
 import matplotlib.pyplot as plt
 
-def sort_sample(Y):
-    if Y < 0.1:
-        return 0
-    elif Y < 0.5:
-        return 1
-    elif Y < 0.9:
-        return 2
-    else:
-        return 3
-
 def main():
     diamonds = data("diamonds")
     diamonds["cut"] = LabelEncoder().fit_transform(diamonds['cut'])
@@ -28,41 +18,36 @@ def main():
     diamonds_for_education = diamonds.loc[:stop, :]
     X = diamonds_for_education.drop(columns=["price"])
     Y = diamonds_for_education.price.values
-    builder = gtapprox.Builder()
+    builder = BuilderWithEncoding()
 
     builder.set_logger(loggers.StreamLogger())
 
     options = {
         "GTApprox/CategoricalVariables": [1, 2, 3],
         "GTApprox/Accelerator": 1,
-        "GTApprox/Technique": "RSM"
+        "GTApprox/Technique": "GBRT"
         #     "GTApprox/GBRTShrinkage": 0.01,
         #     "GTApprox/GBRTSubsampleRatio": 0.1,
         #     "GTApprox/GBRTColsampleRatio": 0.6
     }
 
-    # X.to_numpy()
-    model = builder.build(X, Y, options=options)
-    #
-    # np.vectorize(sort_sample)
-    #
+    model = builder.build_smart(X, Y, options=options)
     # Y = np.random.random(100)
     # X = Y < 0.5
     #
     # builder = gtapprox.Builder()
-    #
     # builder.set_logger(loggers.StreamLogger())
     #
     # model = builder.build(X, Y, options={'GTApprox/CategoricalVariables': [0], 'GTApprox/Technique': 'RSM'})
 
-    diamonds_for_checking = diamonds.loc[stop:, :] # Encoder('binary', cols=diamonds.columns.values[1:4]).
+    if isinstance(builder, BuilderWithEncoding):
+        diamonds_for_checking = Encoder('binary', cols=diamonds.columns.values[1:4]).fit_transform(diamonds.loc[stop:, :])
+    else:
+        diamonds_for_checking = diamonds.loc[stop:, :]
+
     n = 1000
-
-    tests = 100
-
+    tests = 1000
     print(RRMS_calc(model, diamonds_for_checking, n, tests))
-
-
 
 
 if __name__ == '__main__':
