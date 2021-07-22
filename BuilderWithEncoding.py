bd09e9a9ca750a18cc8c34fb0127bf743e03569f
AntonyKon <kononenko150@gmail.com>
2021-07-22 17:32:30 +0300

    add build_smart to BuilderWithEncoding

--- BuilderWithEncoding.py
+++ BuilderWithEncoding.py
@@ -3,15 +3,28 @@ from Encoder import Encoder
 
 
 class BuilderWithEncoding(gtapprox.Builder):
-    def build(self, x, y, options=None, outputNoiseVariance=None, comment=None, weights=None, initial_model=None, annotations=None, x_meta=None, y_meta=None):
+    def __encode_categorial_variables(self, x, options):
         if options:
             categorial_variables = options.get("GTApprox/CategoricalVariables", [])
             if categorial_variables:
-                encoder = Encoder('binary', cols=x.columns.values[categorial_variables[0]: categorial_variables[-1] + 1])
+                encoder = Encoder('binary',
+                                  cols=x.columns.values[categorial_variables[0]: categorial_variables[-1] + 1])
                 x = encoder.fit_transform(x)
 
-        print(x.shape)
+            options["GTApprox/CategoricalVariables"] = []
 
-        options["GTApprox/CategoricalVariables"] = []
+        return x, options
 
-        return super().build(x, y, options, outputNoiseVariance, comment, weights, initial_model, annotations, x_meta, y_meta)
+    def build(self, x, y, options=None, outputNoiseVariance=None, comment=None, weights=None, initial_model=None,
+              annotations=None, x_meta=None, y_meta=None):
+        x, options = self.__encode_categorial_variables(x, options)
+
+        return super().build(x, y, options, outputNoiseVariance, comment, weights, initial_model, annotations, x_meta,
+                             y_meta)
+
+    def build_smart(self, x, y, options=None, outputNoiseVariance=None, comment=None, weights=None, initial_model=None,
+                    hints=None, x_test=None, y_test=None, annotations=None, x_meta=None, y_meta=None):
+        x, options = self.__encode_categorial_variables(x, options)
+
+        return super().build_smart(x, y, options, outputNoiseVariance, comment, weights, initial_model,
+                                   hints, x_test, y_test, annotations, x_meta, y_meta)
