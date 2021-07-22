1927a3349b8b756831e0f26b8c7ac329bcbda671
AntonyKon <kononenko150@gmail.com>
2021-07-22 22:50:15 +0300

    add leaveoneout encoding

--- Encoder.py
+++ Encoder.py
@@ -13,24 +13,31 @@ class Encoder:
         elif isinstance(X, pd.Series):
             self.columns = [X.name]
 
-    def fit(self, X):
+    def fit(self, X, Y=[]):
         if self.columns is None:
             self.__get_columns(X)
 
         return self
 
-    def transform(self, X):
+    def transform(self, X, Y):
         if self.encoding_type == 'binary':
             return self.__transform_with_class(X, ce.OneHotEncoder)
-        elif self.encoding_type == 'mean':
-            pass
+        elif self.encoding_type == 'leave_one_out':
+            return self.__transform_with_class_supervised(X, Y, ce.LeaveOneOutEncoder)
 
-    def fit_transform(self, X):
-        return self.fit(X).transform(X)
+    def fit_transform(self, X, Y=[]):
+        return self.fit(X, Y).transform(X, Y)
 
     def __transform_with_class(self, X, SpecialEncoder):
         X_copy = X.copy()
         encoder = SpecialEncoder(cols=self.columns)
         transformed = encoder.fit(X_copy).transform(X_copy)
+
+        return transformed
+
+    def __transform_with_class_supervised(self, X, Y, SpecialEncoder):
+        X_copy = X.copy()
+        encoder = SpecialEncoder(cols=self.columns)
+        transformed = encoder.fit(X_copy, Y).transform(X_copy)
 
         return transformed
