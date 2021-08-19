46c78959bb8b2dca0500ab8035a00331bf432b8d
AntonyKon <kononenko150@gmail.com>
2021-08-19 18:39:10 +0300

    add deepcopy to avoid problems with links

--- RRMS_checking.py
+++ RRMS_checking.py
@@ -4,6 +4,8 @@ import category_encoders as ce
 from pydataset import data
 import matplotlib.pyplot as plt
 import numpy as np
+from sys import maxsize
+from copy import deepcopy
 
 
 def get_default_model_RRMS(x, y, x_test, y_test, categorical_variables, technique='RSM', binarization=True):
@@ -41,25 +43,27 @@ def get_encoded_model_RRMS(x, y, x_test, y_test, categorical_variables, techniqu
 
 
 def main():
-    tests = 1000
-    dataset = data('diamonds').sample(frac=1, random_state=tests)
+    tests = 100
+    seed = tests // 5
+    dataset = data('diamonds').sample(frac=1, random_state=seed)
+    size = len(dataset)
     categorical_variables = [1, 2, 3]
     y_columns = ["price"]
 
-    np.random.seed(tests)
+    np.random.seed(seed)
 
     print(dataset)
     # test - 10%
     # education - 60-70%
 
-    dataset_for_testing = dataset.tail(100)
-    dataset = dataset.iloc[:-100]
+    dataset_for_testing = dataset.tail(int(size * 0.1))
+    dataset = dataset.iloc[:-int(size * 0.1)]
     x_test = dataset_for_testing.drop(columns=y_columns)
     y_test = dataset_for_testing.loc[:, y_columns]
 
     default_model_RRMS = np.zeros(shape=(tests,))
     encoded_model_RRMS = np.zeros(shape=(tests,))
-    dataset_sizes = np.random.randint(5000, 20000, size=tests)
+    dataset_sizes = np.random.randint(size * 0.6, size * 0.7, size=tests)
 
     for i in range(tests):
         print(i, dataset_sizes[i])
@@ -68,23 +72,25 @@ def main():
         x, y = education.drop(columns=y_columns), education.loc[:, y_columns]
 
         default_model_RRMS[i] = get_default_model_RRMS(
-            x.copy(deep=True), y.copy(deep=True), x_test.copy(deep=True), y_test.copy(deep=True),
+            deepcopy(x), deepcopy(y), deepcopy(x_test), deepcopy(y_test),
             categorical_variables
         )
 
         encoded_model_RRMS[i] = get_encoded_model_RRMS(
-            x.copy(deep=True), y.copy(deep=True), x_test.copy(deep=True), y_test.copy(deep=True),
+            deepcopy(x), deepcopy(y), deepcopy(x_test), deepcopy(y_test),
             categorical_variables
         )
+        print(encoded_model_RRMS[i])
 
     fig, ax = plt.subplots(figsize=(10, 15))
 
     errors = np.unique(np.concatenate([default_model_RRMS, encoded_model_RRMS]))
-    print(list(errors ))
+    # errors = errors[~np.isnan(errors)]
+    print(list(errors))
     encoded_model_count = np.array([np.count_nonzero(encoded_model_RRMS < error) for error in errors])
-    default_model_RRMS = np.array([np.count_nonzero(default_model_RRMS < error) for error in errors])
+    default_model_count = np.array([np.count_nonzero(default_model_RRMS < error) for error in errors])
 
-    ax.plot(errors, default_model_RRMS, label="Стандартная модель")
+    ax.plot(errors, default_model_count, label="Стандартная модель")
     ax.plot(errors, encoded_model_count, label="Модель с кодированием")
     ax.grid()
     ax.legend()
@@ -92,12 +98,5 @@ def main():
     plt.show()
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
     main()
-
