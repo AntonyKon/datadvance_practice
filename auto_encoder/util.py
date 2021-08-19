d7fdf0cd98485d324fe5e8d6f99590639f54341c
AntonyKon <kononenko150@gmail.com>
2021-08-19 18:38:19 +0300

    new version of configuration searching algorithm

--- auto_encoder/util.py
+++ auto_encoder/util.py
@@ -2,7 +2,16 @@ import category_encoders as ce
 from .encoders import DummyEncoder, SubsampleEncoder
 import numpy as np
 import pandas as pd
+from scipy.stats import chisquare
 from collections import defaultdict
+
+
+def _chisquare(observed, expected=None):
+    observed = pd.Series(observed).to_numpy(dtype=float)
+    if expected is None:
+        expected = np.full_like(observed, np.mean(observed), dtype=float)
+
+    return np.sum(((observed - expected) ** 2) / expected)
 
 
 class ConfigurationManager:
@@ -14,94 +23,149 @@ class ConfigurationManager:
 
     class metrics:
 
-        def __init__(self, max_shape, min_elements):
-            self.max_shape = max_shape
+        def __init__(self, initial_dim, min_elements, max_elements=None):
+            self.initial_dim = initial_dim
             self.min_elements = min_elements
+            self.max_elements = max_elements
 
-        def std_limit(self, df):
-            return max(0, df.std() - np.log2((df.max() - df.min()) + 1) * 225)
+        def disribution_metric(self, subsample_sizes):  # 704.17172013
+            return _chisquare(subsample_sizes)
 
-        def elements_limit(self, df):
-            return np.count_nonzero(df.to_numpy() < self.min_elements) * 100
+        def elements_limit(self, subsample_sizes):  # 944.24572173
+            mask = self.__elements_min_limit(subsample_sizes)
 
-        def dimension_limit(self, dimension):
-            return max(0, dimension - self.max_shape) * 100
+            if self.max_elements:
+                mask += self.__elements_max_limit(subsample_sizes)
 
-        def data_losing(self, transformed_values, column_values):
-            return (column_values.nunique() - transformed_values.nunique()) * 100
+            return np.count_nonzero(mask)
 
-        def diff_limit(self, transformed_values):
+        def sample_count_metric(self, subsamples):  # 1.96859905
+            return len(subsamples) - 1
+
+        def __elements_min_limit(self, subsample_sizes):
+            return subsample_sizes.to_numpy() < self.min_elements
+
+        def __elements_max_limit(self, subsample_sizes):
+            return subsample_sizes.to_numpy() > self.max_elements
+
+        def dimension_increase(self, encoded_dim):  # 134.96166354
+            return encoded_dim / self.initial_dim - 1
+            # candidate_encoders = candidate_encoders.filter(lambda _: subsample_sizes.min() > mni_elements)
+            # penalty = coef1 * std_metric + coef2 * dimension_metric + coefN * len(subsample_sizes)
+            # return max(0, dimension - self.max_shape) * 100
+
+        def data_losing(self, transformed_values, column_values):  # 539.48631667
+            return abs(len(column_values) - len(transformed_values))
+
+        def diff_metric(self, transformed_values):  # 29.11024482
             unique_elements = transformed_values.unique()
             unique_count = transformed_values.nunique()
 
-            real_diff = np.median(np.abs(np.diff(unique_elements)))  # calculating median of real differences
-            ideal_diff = (np.max(unique_elements) - np.min(unique_elements)) / (
-                        unique_count - 1)  # arithmetic progression (calculating d)
+            real_diff = np.abs(np.diff(unique_elements))  # calculating median of real differences
+            ideal_diff = np.ptp(unique_elements) / (unique_count - 1)  # arithmetic progression (calculating d)
 
-            return max(0, real_diff - ideal_diff)
+            return _chisquare(real_diff, np.full_like(real_diff, ideal_diff))
 
-    def __init__(self, x, y, encoders=None):
-        self.encoders = encoders
+    def __init__(self, x, y, encoders=None, technique=None):
+        self.encoders = np.array(encoders)
         self.x = pd.DataFrame(x)
         self.y = y
 
-        if self.encoders is None:
-            self.encoders = [SubsampleEncoder, DummyEncoder, ce.LeaveOneOutEncoder, ce.BinaryEncoder]
+        if encoders is None:
+            self.encoders = np.array([SubsampleEncoder, DummyEncoder, ce.LeaveOneOutEncoder, ce.BinaryEncoder])
+
+        y_shape = 1 if len(y.shape) == 1 else y.shape[1]
+
+        min_size = 2 * (self.x.shape[1] + y_shape) + 1
+        max_size = None
+
+        if technique == 'GP' or technique == 'HDAGP':
+            max_size = 4000
+
+        self.metrics_checker = self.metrics(self.x.shape[1], min_size, max_size)
 
     def build_configuration(self, categorical_variables):
         if len(categorical_variables) == 0:
             return []
 
-        categorical_variables = self.x.iloc[:, categorical_variables].columns.to_numpy()
-        actual_shape = self.x.shape[1]
-        metric_checker = self.metrics(max_shape=2.5 * actual_shape, min_elements=2 * actual_shape + 4)
+        categorical_variables = self.x.iloc[:, categorical_variables].columns.to_numpy(dtype=str)
+        encoders_num = len(self.encoders)
+        steps = 3 * encoders_num
+        coeff = [83.24192459, 0.0152112, 84.34873041, 492.72273704, 343.75608215, 1000000175.1594066]
+        probability_for_best = 0.7
+        prediction = np.zeros_like(categorical_variables, dtype=int)
+        probability_for_other = (1 - probability_for_best) / (len(prediction) - 1)
+        configuration = self.__configure_encoders(prediction, categorical_variables)
+        min_penalty = self.__calculate_penalty(configuration, coeff)
 
-        configuration = defaultdict(list)
+        for _ in range(steps):
+            penalties = np.zeros_like(prediction, dtype=float)
+            for i in range(len(prediction)):
+                tmp = prediction[i]
+                prediction[i] = (prediction[i] + 1) % encoders_num
+                configuration = self.__configure_encoders(prediction, categorical_variables)
+                penalties[i] = self.__calculate_penalty(configuration, coeff)
+                prediction[i] = tmp
 
-        for column in categorical_variables:
-            scores = dict()
+            i = np.argmin(penalties)
+            if penalties[i] > min_penalty:
+                break
 
-            for encoder in self.encoders:
-                all_columns = None
-                if isinstance(encoder(), SubsampleEncoder):
-                    all_columns = configuration[SubsampleEncoder] + [column]
+            probabilities = np.full_like(prediction, probability_for_other, dtype=float)
+            probabilities[i] = probability_for_best
+            i = np.random.choice(len(prediction), p=probabilities)
+            prediction[i] = (prediction[i] + 1) % encoders_num
+            min_penalty = penalties[i]
 
-                scores[encoder] = self.__validate(encoder, metric_checker, actual_shape, column, all_columns=all_columns)
+        return self.__configure_encoders(prediction, categorical_variables)
 
-            optimal_encoder = min(scores, key=scores.get)
+    def __calculate_penalty(self, configuration, coeff):
+        penalty = 0
+        configuration = [encoder.fit(self.x, self.y) for encoder in configuration]
 
-            if isinstance(optimal_encoder(), SubsampleEncoder):
-                configuration[ce.OrdinalEncoder].append(column)
+        initial_columns = set(self.x.columns)
 
+        columns = set(self.x.columns)
+        columns_to_delete = None
 
-            configuration[optimal_encoder].append(column)
+        for encoder in configuration:
+            transformed = encoder.transform(self.x, self.y)
+            sample_sizes = pd.Series([len(transformed)])
+            transformed_columns = set(encoder.get_feature_names()) - initial_columns
 
-            tmp_transformed = optimal_encoder(column).fit_transform(self.x, self.y)
-            actual_shape += tmp_transformed.columns.size - self.x.columns.size
+            penalty += coeff[0] * self.metrics_checker.data_losing(
+                transformed.loc[:, transformed_columns or encoder.cols].value_counts(),
+                self.x.loc[:, encoder.cols].value_counts())
 
-            dict_for_sort = defaultdict(int)
-            dict_for_sort[SubsampleEncoder] = 2
-            dict_for_sort[ce.OrdinalEncoder] = 1
+            if not transformed_columns:
+                for col in encoder.cols:
+                    penalty += coeff[1] * self.metrics_checker.diff_metric(transformed[col])
 
-        return sorted([encoder(cols=configuration[encoder]) for encoder in configuration],
-                      key=lambda encoder: dict_for_sort[type(encoder)])
+            if isinstance(encoder, SubsampleEncoder):
+                columns_to_delete = set(encoder.cols)
+                sample_sizes = self.x.groupby(encoder.cols).size()
 
-    def __validate(self, encoder, metric_checker, actual_shape, column, all_columns=None):
-        score = 0
-        dataset_size = pd.Series([len(self.x)])
+            columns ^= set(encoder.get_feature_names())
 
-        if isinstance(encoder(), SubsampleEncoder):
-            dataset_size = self.x.groupby(all_columns).size()
-            score += metric_checker.std_limit(dataset_size)
+            penalty += coeff[2] * self.metrics_checker.disribution_metric(sample_sizes)
+            penalty += coeff[3] * self.metrics_checker.sample_count_metric(sample_sizes)
+            penalty += coeff[4] * self.metrics_checker.elements_limit(sample_sizes)
 
-        score += metric_checker.elements_limit(dataset_size)
-        tmp_transformed = encoder(cols=column).fit(self.x, self.y).transform(self.x) # leaveoneout turns into target encoder without y
-        actual_shape += tmp_transformed.shape[1] - self.x.shape[1]
+        if columns_to_delete:
+            columns -= columns_to_delete
 
-        score += metric_checker.dimension_limit(actual_shape)
+        penalty += coeff[5] * self.metrics_checker.dimension_increase(len(columns))
 
-        if column in tmp_transformed.columns:
-            score += metric_checker.data_losing(tmp_transformed[column], self.x[column])
-            score += metric_checker.diff_limit(tmp_transformed[column])
+        return penalty
 
-        return score
+    def __configure_encoders(self, encoder_numbers, cols=None):
+        raw_classes = self.encoders[encoder_numbers]
+        encoders_cols = {cls: cols[np.where(raw_classes == cls)[0]] for cls in raw_classes}
+        result = sorted((encoder(cols=encoders_cols[encoder]) for encoder in encoders_cols),
+                        key=lambda encoder: 1 if isinstance(encoder, SubsampleEncoder) else 0)
+
+        if isinstance(result[-1], SubsampleEncoder):
+            result.append(ce.OrdinalEncoder(cols=result[-1].cols))
+            result[-1], result[-2] = result[-2], result[-1]
+
+        return result
